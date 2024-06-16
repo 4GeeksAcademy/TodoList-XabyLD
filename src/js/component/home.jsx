0f729733e48a63830eb47c1d3ca5e8a00d96922d
Longodios <tusaromix@gmail.com>
2024-06-16 23:38:13 +0200

    Añado funciones de usuario, corrigo errores , finalizado el proyecto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,9 +2,8 @@ import React, { useEffect, useState } from "react";
 
 /*
 TODO  
---> Hacer que cuando entro a un usuario pueda crear tareas
---> Revisar botón para que SOLO mande la notificación de las tareas borradas
---> Revisar la creación de usuarios
+--> Revisar botón para que SOLO mande la notificación 1 vez de las tareas borradas
+
 */
 
 //create your first component
@@ -12,15 +11,19 @@ const Home = () => {
   const [task, SetTask] = useState("");
   const [tasks, SetTasks] = useState([]);
   const [countLi, SetCountLi] = useState(0);
-  const [username, setUsername] = useState("XabyLD");
+  const [username, setUsername] = useState("");
   //const [isEnter, setIsEnter] = useState(false);
   const [lastUsername, setLastUsername] = useState("");
   const [idTask, setIdTask] = useState(0);
+  //Visualizamos usuario activo
+  console.log(lastUsername);
+  //Visualizamos array de tareas
+  console.log(tasks);
 
-  const toDoGetTasks = async () => {
+  const toDoGetTasks = async (lastUsername) => {
     try {
       const response = await fetch(
-        `https://playground.4geeks.com/todo/users/${username}`,
+        `https://playground.4geeks.com/todo/users/${lastUsername}`,
         {
           method: "GET",
           headers: {
@@ -31,16 +34,18 @@ const Home = () => {
 
       const result = await response.json();
 
-      // console.log(result); //Enseñamos el usuario con las tareas por consola
-      SetTasks(result.todos);
+      console.log(result); //Enseñamos el usuario con las tareas por consola
+      if (!result.todos) return; //Si no tenemos tareas no hacemos nada
+      SetTasks(result.todos); // Si existen tareas setea las tareas.
     } catch (error) {
       console.log(error);
     }
   };
   const toDoUsers = async (toDo) => {
+    console.log(toDo); //Tarea que le llega al método POST de la API
     try {
       const response = await fetch(
-        `https://playground.4geeks.com/todo/todos/${username}`,
+        `https://playground.4geeks.com/todo/todos/${lastUsername}`,
         {
           method: "POST",
           headers: {
@@ -50,17 +55,23 @@ const Home = () => {
           body: JSON.stringify(toDo),
         }
       );
-
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
       const result = await response.json();
+      console.log(result); //Tenemos la tarea
+      /* SetTasks((prevTasks) => [...prevTasks, result.label]);
+      return result; */
     } catch (error) {
       console.log(error);
     }
   };
 
-  const createUsernameAPI = async (userName) => {
+  const createUsernameAPI = async (lastUsername) => {
+    console.log(lastUsername);
     try {
       const response = await fetch(
-        `https://playground.4geeks.com/todo/users/${userName}`,
+        `https://playground.4geeks.com/todo/users/${lastUsername}`,
 
         {
           method: "POST",
@@ -70,6 +81,9 @@ const Home = () => {
         }
       );
       const resultUser = await response.json();
+      if (resultUser.details === "User already exists") {
+        toDoGetTasks(lastUsername);
+      }
       console.log(resultUser);
     } catch (error) {
       console.log(error);
@@ -79,7 +93,6 @@ const Home = () => {
   const toDoPutUsers = async (id) => {
     try {
       const toDo = tasks.find((task) => task.id == id);
-      console.log(id);
 
       toDo.is_done = true;
       const response = await fetch(
@@ -94,6 +107,7 @@ const Home = () => {
         }
       );
       const result = await response.json();
+      console.log(result);
     } catch (error) {
       console.log(error);
     }
@@ -120,9 +134,9 @@ const Home = () => {
           },
         }
       );
-      if (response.ok == true) {
+      /* if (response.ok == true) {
         alert("La tarea se elimino correctamente");
-      }
+      } */
     } catch (error) {
       console.log(error);
     }
@@ -142,7 +156,7 @@ const Home = () => {
   const saveUsername = (event) => {
     if (event.key === "Enter") {
       setLastUsername(username);
-      createUsernameAPI(username);
+      createUsernameAPI(event.target.value);
       alert("Username Guardado");
     }
   };
@@ -153,37 +167,45 @@ const Home = () => {
   }, [tasks]);
 
   useEffect(() => {
-    const handleKeyUp = (event) => {
-      //Nos aseguramos que la tarea no sea en blanco para que NO la añada y que pulse la tecla enter para añadirla
-      if (event.key === "Enter" && task.trim() !== "") {
-        SetTasks([...tasks, task]);
-        const newTask = { label: task, is_done: false, id: task.label };
-        toDoUsers(newTask);
-        //setIsEnter(!isEnter);
-        SetTask("");
-        toDoGetTasks();
-      }
-    };
-    document.addEventListener("keyup", handleKeyUp);
+    if (!lastUsername) {
+      return "el usuario está vacío";
+    } else {
+      const handleKeyUp = (event) => {
+        //Nos aseguramos que la tarea no sea en blanco para que NO la añada y que pulse la tecla enter para añadirla
+        if (event.key === "Enter" && task.trim() !== "") {
+          // SetTasks([...tasks, task]);
+          console.log(task);
+          const newTask = { label: task, is_done: false, id: task.id };
+          console.log(newTask);
+          toDoUsers(newTask);
+          SetTasks((prevTasks) => [...prevTasks, newTask]);
+          SetTask("");
 
-    return () => {
-      document.removeEventListener("keyup", handleKeyUp);
-    };
-  }, [task, tasks]);
+          //setIsEnter(!isEnter);
+        }
+      };
+      document.addEventListener("keyup", handleKeyUp);
+
+      return () => {
+        document.removeEventListener("keyup", handleKeyUp);
+      };
+    }
+  }, [task]);
 
   useEffect(() => {
     toDoPutUsers();
   }, []);
 
   useEffect(() => {
-    toDoGetTasks();
-  }, []);
+    if (!lastUsername) return;
+    toDoGetTasks(lastUsername);
+  }, [lastUsername]);
   //Creación de Usuario en API
-  useEffect(() => {
+  /*  useEffect(() => {
     const handleKeyUp = (event) => {
       if (event.key === "Enter" && username.trim() !== "") {
         const newUser = username;
-        //createUsernameAPI(newUser);
+        // createUsernameAPI(newUser);
         setUsername("");
       }
     };
@@ -192,7 +214,7 @@ const Home = () => {
     return () => {
       document.removeEventListener("keyup", handleKeyUp);
     };
-  }, [username]);
+  }, [username]); */
 
   return (
     <>
@@ -220,36 +242,41 @@ const Home = () => {
           />
         </div>
       </div>
-      <ul
-        id="myUl"
-        className="list-group list-unstyled  text-xl d-flex flex-wrap align-content-center"
-      >
-        {tasks.map((item, index) => (
-          <li
-            className=" mt-0 fs-1 border border-1 w-75 d-flex justify-content-between "
-            key={index}
-            id={item.id}
-          >
-            <span>{item.label}</span>
-            <div className="d-flex gap-3">
-              <button
-                onClick={() => toDoPutUsers(item.id)}
-                type="button"
-                className="btn btn-success btn-md"
-              >
-                <i className="fa-solid fa-check"></i>
-              </button>
-              <button
-                onClick={() => toDoDeleteTask(item.id)}
-                type="button"
-                className="btn btn-danger btn-md"
-              >
-                <i className="fa-solid fa-trash "></i>
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
+      {tasks.length === 0 ? (
+        <p className="text-center fs-2">Sin tareas</p>
+      ) : (
+        <ul
+          id="myUl"
+          className="list-group list-unstyled  text-xl d-flex flex-wrap align-content-center"
+        >
+          {tasks.map((item, index) => (
+            <li
+              className=" mt-0 fs-1 border border-1 w-75 d-flex justify-content-between "
+              key={index}
+              id={item.id}
+            >
+              <span>{item.label}</span>
+
+              <div className="d-flex gap-3">
+                <button
+                  onClick={() => toDoPutUsers(item.id)}
+                  type="button"
+                  className="btn btn-success btn-md"
+                >
+                  <i className="fa-solid fa-check"></i>
+                </button>
+                <button
+                  onClick={() => toDoDeleteTask(item.id)}
+                  type="button"
+                  className="btn btn-danger btn-md"
+                >
+                  <i className="fa-solid fa-trash "></i>
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
       <div className=" d-flex justify-content-between w-75 py-2 px-2">
         <p className="py-2 px-4">Total items: {countLi}</p>
         <button
